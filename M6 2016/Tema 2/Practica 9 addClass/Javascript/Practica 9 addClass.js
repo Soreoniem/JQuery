9ac6686562c49d423971be6e6f404a197aea652f
Soreoniem <j_l_juan@hotmail.com>
2016-11-22 16:10:02 +0100

    AddClass
    Añadida la práctica del AddClass

---
+++ M6 2016/Tema 2/Practica 9 addClass/Javascript/Practica 9 addClass.js
@@ -0,0 +1,50 @@
+﻿/*
+	Descripción de la práctica
+	• 
+*/
+$(document).ready(function(){
+	// Variables de control
+	var thead	= false;
+	var tbody	= false;
+	
+	// Click en el botón Thead
+	$("#modThead").click(function(){
+		// Almacena todos los tags de thead (1 esta vez)
+		var todosThead	= $("thead");
+		
+		// If activado (variable thead)
+		if(thead){
+			// cambio valor de control
+			thead	= false;
+			
+			// Eliminar la clase
+			todosThead.removeClass("cssThead");
+		} else {
+			// cambio valor de control
+			thead	= true;
+			
+			// Añadir la clase
+			todosThead.addClass("cssThead");
+		}
+	});
+	
+	// Click en el botón Tbody
+	$("#modTbody").click(function(){
+		// Almacena todos los tags de tbody(1)
+		var todosTbody	= $("tbody");
+		
+		// If activado (variable tbody)
+		if(tbody){
+			// cambio valor de control
+			tbody	= false;
+			
+			// Eliminar la clase
+			todosTbody.removeClass("cssTbody");
+		} else {
+			// cambio valor de control
+			tbody	= true;
+			// Añadir la clase
+			todosTbody.addClass("cssTbody");
+		}
+	});
+});
