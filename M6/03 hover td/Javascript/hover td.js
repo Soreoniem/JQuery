0cf3d4b32a96a7b0aea4afa08349257d77259d1b
Juan Lu <j_l_juan@hotmail.com>
2017-03-31 08:34:11 +0200

    Practica solo javascript
    Practica de javascript hover td
    Si haces click cambia el color

---
+++ M6/03 hover td/Javascript/hover td.js
@@ -0,0 +1,26 @@
+﻿var colores = ["black", "yellow", "red", "blue", "green", "white"];
+var idColor = 0;
+
+function iniciarScript(){
+	var tabla = "<table style=\"text-align:center; border-collapse:collapse;\">";
+	var tablaAnchura	= 50;
+
+	for( var i=1 ; i<=tablaAnchura ; i++ ){
+		tabla += "<tr>";
+			for( var j=1 ; j<=tablaAnchura ; j++ ){
+				tabla += "<td onmouseover=\"colorear(this)\"></td>";
+			}
+		tabla += "</tr>";
+	}
+	tabla += "</table>";
+    document.getElementsByTagName("body")[0].innerHTML = tabla;
+}
+
+function colorear(esteTD){
+	esteTD.style.backgroundColor = colores[idColor];
+}
+
+function cambioColor(){
+	idColor++;
+	if( idColor >= colores.length ){ idColor = 0; }
+}
